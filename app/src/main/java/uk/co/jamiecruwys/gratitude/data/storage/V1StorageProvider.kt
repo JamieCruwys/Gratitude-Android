b73afc58dab8b1454a518d6f70cc6a88b1e37a5d
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-04-06 23:19:21 +0100

    Added v1 storage mechanism

---
+++ app/src/main/java/uk/co/jamiecruwys/gratitude/data/storage/V1StorageProvider.kt
@@ -0,0 +1,53 @@
+package uk.co.jamiecruwys.gratitude.data.storage
+
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.JsonParseException
+import com.google.gson.reflect.TypeToken
+import uk.co.jamiecruwys.gratitude.data.model.GratitudeEntry
+import java.util.*
+
+/**
+ * Storage provider from version 1 that persists gratitude entries using shared preferences
+ */
+class V1StorageProvider(context: Context): VersionedStorageProviderContract
+{
+	object Companion {
+		const val V1_SHARED_PREFERENCES_NAME = "gratitude_preferences"
+		const val V1_SHARED_PREFERENCES_KEY = "gratitude_entries"
+	}
+
+	private val gson = Gson()
+	private val sharedPreferences = context.getSharedPreferences(Companion.V1_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+	override fun load(): MutableList<GratitudeEntry>
+	{
+		val json = sharedPreferences.getString(Companion.V1_SHARED_PREFERENCES_KEY, null)
+
+		return try
+		{
+			val data: MutableList<String> = Gson().fromJson(json, object : TypeToken<Collection<String>>() {}.type)
+			data.mapTo(ArrayList(), { GratitudeEntry(text = it) })
+		}
+		catch (e: JsonParseException)
+		{
+			Log.e(TAG, "Failed to get gratitude entries from JSON")
+			ArrayList()
+		}
+	}
+
+	override fun hasData(): Boolean = !sharedPreferences.getString(Companion.V1_SHARED_PREFERENCES_KEY, null).isNullOrBlank()
+
+	override fun save(items: MutableList<GratitudeEntry>)
+	{
+		val json = gson.toJson(items)
+		sharedPreferences.edit().putString(Companion.V1_SHARED_PREFERENCES_KEY, json).apply()
+	}
+
+	override fun destroy()
+	{
+		sharedPreferences.edit().clear().apply()
+	}
+}
