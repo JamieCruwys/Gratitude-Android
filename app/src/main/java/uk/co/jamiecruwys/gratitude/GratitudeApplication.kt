8593433df981434be988401295991dbd3eafedc1
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-04-06 23:20:45 +0100

    Now triggering migrator

--- app/src/main/java/uk/co/jamiecruwys/gratitude/GratitudeApplication.kt
+++ app/src/main/java/uk/co/jamiecruwys/gratitude/GratitudeApplication.kt
@@ -1,5 +1,13 @@
 package uk.co.jamiecruwys.gratitude
 
 import android.app.Application
+import uk.co.jamiecruwys.gratitude.data.migration.GratitudeMigrator
 
 class GratitudeApplication : Application()
+{
+	override fun onCreate()
+	{
+		super.onCreate()
+		GratitudeMigrator.migrate(this)
+	}
+}
