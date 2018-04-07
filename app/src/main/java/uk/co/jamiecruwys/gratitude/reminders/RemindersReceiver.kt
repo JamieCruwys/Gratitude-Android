f31d1846cf543a7240b1530b93ea686824f5e9d1
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-04-07 02:34:09 +0100

    Switched over

---
+++ app/src/main/java/uk/co/jamiecruwys/gratitude/reminders/RemindersReceiver.kt
@@ -0,0 +1,13 @@
+package uk.co.jamiecruwys.gratitude.reminders
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class RemindersReceiver : BroadcastReceiver()
+{
+	override fun onReceive(context: Context?, intent: Intent?)
+	{
+		TODO("not implemented")
+	}
+}
