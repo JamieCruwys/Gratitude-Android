a7790075b41def4b83cff21bbeb67499af89ef6c
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-04-08 01:22:18 +0100

    Updated reminder provider contract

--- app/src/main/java/uk/co/jamiecruwys/gratitude/reminders/ReminderProviderContract.kt
+++ app/src/main/java/uk/co/jamiecruwys/gratitude/reminders/ReminderProviderContract.kt
@@ -1,17 +1,10 @@
 package uk.co.jamiecruwys.gratitude.reminders
 
-import java.util.*
-
 /**
  * Contract for triggering reminders
  */
 interface ReminderProviderContract
 {
-	/**
-	 * Sets a number of numbers that the user should be reminded
-	 */
-	fun setReminderTimes(vararg time: Calendar?)
-
 	/**
 	 * Remind the user from now on
 	 */
