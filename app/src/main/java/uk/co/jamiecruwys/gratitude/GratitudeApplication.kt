1018e77166db23ea21125fbbb37f3e1460935db1
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-04-08 19:41:30 +0100

    Creating a notification channel when launching on android oreo

--- app/src/main/java/uk/co/jamiecruwys/gratitude/GratitudeApplication.kt
+++ app/src/main/java/uk/co/jamiecruwys/gratitude/GratitudeApplication.kt
@@ -1,7 +1,9 @@
 package uk.co.jamiecruwys.gratitude
 
 import android.app.Application
+import android.os.Build
 import uk.co.jamiecruwys.gratitude.data.migration.GratitudeMigrator
+import uk.co.jamiecruwys.gratitude.notifications.NotificationSender
 import uk.co.jamiecruwys.gratitude.reminders.ReminderSender
 
 /**
@@ -13,6 +15,10 @@ class GratitudeApplication : Application()
 	{
 		super.onCreate()
 		GratitudeMigrator.migrate(this)
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+		{
+			NotificationSender.createNotificationChannel(this)
+		}
 		ReminderSender(this).startReminding()
 	}
 }
